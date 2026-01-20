cca20230e77909a417cc5ed0b42eb9d31850bf58
baom6c-rgb <baom6c@gmail.com>
2026-01-20 12:14:52 +0700

    firt commit

---
+++ src/main/java/com/be_ai_learning_platform/BeAiLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.be_ai_learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAiLearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAiLearningPlatformApplication.class, args);
+    }
+
+}
