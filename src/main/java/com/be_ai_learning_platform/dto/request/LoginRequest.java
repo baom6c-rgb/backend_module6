48d75487df5476d95d546c2b3c2e24a7fbf984c4
onion123442134 <hoanganhnd2003@gmail.com>
2026-01-22 08:48:24 +0700

    update-valid

--- src/main/java/com/be_ai_learning_platform/dto/request/LoginRequest.java
+++ src/main/java/com/be_ai_learning_platform/dto/request/LoginRequest.java
@@ -1,14 +1,15 @@
 package com.be_ai_learning_platform.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
-
-    @NotBlank
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không đúng định dạng")
     private String email;
 
-    @NotBlank
+    @NotBlank(message = "Mật khẩu không được để trống")
     private String password;
 }
