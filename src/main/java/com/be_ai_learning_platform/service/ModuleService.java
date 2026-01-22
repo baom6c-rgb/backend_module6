04e6262befa439d0508e35e1d5a24a03486c52ef
onion123442134 <hoanganhnd2003@gmail.com>
2026-01-22 17:27:31 +0700

    up service

---
+++ src/main/java/com/be_ai_learning_platform/service/ModuleService.java
@@ -0,0 +1,9 @@
+package com.be_ai_learning_platform.service;
+
+import com.be_ai_learning_platform.entity.LearningModule;
+
+import java.util.List;
+
+public interface ModuleService {
+    List<LearningModule> getAllModules();
+}
